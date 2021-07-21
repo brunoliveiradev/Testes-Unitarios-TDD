d995a7a32bf6023add34a5ba0bd835c68db3b881
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-07-21 16:11:40 -0300

    Test cases using ParameterizedTest.

--- src/test/java/com/brunoliveiradev/tdd/service/BonusServiceTest.java
+++ src/test/java/com/brunoliveiradev/tdd/service/BonusServiceTest.java
@@ -1,12 +1,18 @@
 package com.brunoliveiradev.tdd.service;
 
 import com.brunoliveiradev.tdd.model.Funcionario;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -44,5 +50,25 @@ class BonusServiceTest {
         BigDecimal bonus = service.calcularBonus(funcionario);
 
         assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+
+    @DisplayName("Testando o método ParameterizedTest usando Value Source")
+    @ParameterizedTest
+    @ValueSource(strings = {"10001", "25000", "30000"})
+    void testandoUsandoParameterizedTestComValueSource(String salario){
+        Funcionario funcionario = new Funcionario("funcionário", LocalDate.now(), new BigDecimal(salario));
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(funcionario));
+    }
+
+    @DisplayName("Testando o método ParameterizedTest usando Method Source")
+    @ParameterizedTest
+    @MethodSource("valoresSalarios")
+    void testandoUsandoParameterizedTestComMethodSource(String salario){
+        Funcionario funcionario = new Funcionario("funcionário", LocalDate.now(), new BigDecimal(salario));
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(funcionario));
+    }
+
+    private static List<String> valoresSalarios(){
+        return Arrays.asList("10001", "25000", "30000");
     }
 }
